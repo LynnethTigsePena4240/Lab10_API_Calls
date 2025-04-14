aee9e6e4ce97deae54bb1b9cad60e16ebccdc783
lynneth <lynneth_tigsepena@hotmail.com>
2025-04-14 15:21:12 -0400

    completed task 2 doing get request using XHR

--- script.js
+++ script.js
@@ -18,3 +18,20 @@ function displayData(data) {
     const displayDiv = document.getElementById('display_text');
     displayDiv.innerHTML = `<h2>${data.title}</h2><p>${data.body}</p>`;
 }
+
+document.getElementById("XHR").addEventListener("click", function() {
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', 'https://jsonplaceholder.typicode.com/posts/2', true);
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4) { // self note 4 means the request is done
+            if (xhr.status === 200) { // and 200 means the request was successful
+                const data = JSON.parse(xhr.responseText);
+                console.log(data);
+                displayData(data);
+            } else {
+                console.error('Error fetching data:', xhr.statusText);
+            }
+        }
+    };
+    xhr.send();
+})
